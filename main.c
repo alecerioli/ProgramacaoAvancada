56851e0d98f2026b88ed03eae8d4a2c9ab265fe9
alecerioli <51276076+alecerioli@users.noreply.github.com>
2019-08-17 17:59:43 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    //1---Ponteiros parecem ser apenas endereços de memoria,
+    //porém é evidente sua enorme variadade de aplicações.
+    //Desde alocação dinamica, oque permite um uso mais eficiente da memoria,
+    //até o uso desses ponteiros para tarefas mais complexas,
+    //como tecnicas de engenharia de softwares.
+
+    return 0;
+}
