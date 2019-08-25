98b40053181d6fcdce32d0c86b3735e50edfbba5
alecerioli <51276076+alecerioli@users.noreply.github.com>
2019-08-25 15:44:40 -0300

    Update main_questao1.c

--- Lista1/Questao01/main_questao1.c
+++ Lista1/Questao01/main_questao1.c
@@ -2,9 +2,12 @@
 
 int main()
 {
-    //1---Ponteiros parecem ser apenas endereços de memoria,
-    //porém é evidente sua enorme variadade de aplicações.
-    //Desde alocação dinamica, oque permite um uso mais eficiente da memoria,
+    //Na vida de engenheiro e de programador é comum deparar-se com
+    //programas muito extensos e utilizar variaveis alocadas estaticamente
+    //nao será tao interessante devido a grande quantidade de memoria que será
+    //utilizada. Ponteiros parecem ser apenas endereços de memoria,
+    //porém é evidente sua enorme variadade de aplicações, desde alocação dinamica,
+    //oque permite um uso mais eficiente da memoria em relacao a alocacao estatica,
     //até o uso desses ponteiros para tarefas mais complexas,
     //como tecnicas de engenharia de softwares.
 
