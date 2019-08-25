5d124a6e87415e792551afee9c4e2e91799e0896
alecerioli <51276076+alecerioli@users.noreply.github.com>
2019-08-25 15:57:44 -0300

    Update main_questao10.c

--- Lista1/Questao10/main_questao10.c
+++ Lista1/Questao10/main_questao10.c
@@ -10,10 +10,10 @@ int main()
     //  x+1 será 4094, x+2 será 4096 e x+3 será 4098,
     //  isso porque um int ocupa 2 bytes.
     //--x for declarado como float:
-    //  x+1 será 4096, x+2 será 5000 e x+3 será 5004,
+    //  x+1 será 4096, x+2 será 4100 e x+3 será 4104,
     //  isso porque um float ocupa 4 bytes.
     //--x for declarado como double:
-    //  x+1 será 5000, x+2 será 5008 e x+3 será 5016,
+    //  x+1 será 4100, x+2 será 4108 e x+3 será 4116,
     //  isso porque um double ocupa 8 bytes.
     return 0;
 }
