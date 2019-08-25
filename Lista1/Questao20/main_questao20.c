b8aedce890e7a656380831a31e16fc73c2bb3460
alecerioli <51276076+alecerioli@users.noreply.github.com>
2019-08-25 16:34:35 -0300

    Update main_questao20.c

--- Lista1/Questao20/main_questao20.c
+++ Lista1/Questao20/main_questao20.c
@@ -5,11 +5,17 @@
 int main (){
     int i,n;
     n = 1000;
+    GC_INIT();
     for (i=0; i<n; i++){
-        int *q = (int*)malloc(sizeof(int));
+            int **ptr = (int **) GC_MALLOC(sizeof(int*));
+            int *qtr = (int*) GC_MALLOC_ATOMIC(sizeof(int));
+            *ptr = (int*) GC_REALLOC(qtr, 2*sizeof(int));
+        //Realocamos o ponteiro '*ptr', para assim o programa
+        //"apagar" automaticamente o ponteiro '*qtr' que nao será
+        //mais utilizado
     }
-    //Atravez da biblioteca libGC torna-se desnecessario
-    //o uso da funcão free, mesmo alocando um numero MUITO
-    //grande de memoria (1000 alocacoes);
+    //Usando as funcoes da biblioteca libGC torna-se desnecessario
+    //o uso da funcão free, mesmo alocando e realocando um numero MUITO
+    //grande de memoria (1000 alocacoes e realocacoes).
     return 0;
 }
